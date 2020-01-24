fd3830426fafb9b9b0330d75899cf4b67e324ee9
p.singh <p.singh@synchronoss.com>
2020-01-24 19:12:15 +0530

    info

---
+++ movie-info-service/src/main/java/com/jdi/test/movieinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,15 @@
+package com.jdi.test.movieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+	
+	
+}
